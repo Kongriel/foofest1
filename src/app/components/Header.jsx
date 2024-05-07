4ede89e33d9d8e1c4470bb547fa9c63fab63bc8e
Anton <krauseriel@gmail.com>
2024-05-07 15:26:26 +0200

    hello

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,15 +4,27 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <div className=" shadow-glass-1 px-4 rounded-lg bg-white bg-opacity-50">
-      <ul className="flex justify-between items-center">
-        <li className="p-3">
-          <Link href={"/"}>
-            <Image src="/diversa.svg" alt="Diversa Logo" width={140} height={75} />
+    <div className="bg-white bg-opacity-50 shadow-lg px-4 py-2 rounded-lg flex justify-between items-center">
+      <Link href="/" passHref>
+        <div className="flex items-center cursor-pointer">
+          <Image src="/diversa.svg" alt="Diversa Logo" width={140} height={75} />
+        </div>
+      </Link>
+      <ul className="flex space-x-4">
+        <li>
+          <Link href="/bands" className="text-xl hover:text-blue-700 cursor-pointer">
+            Bands
           </Link>
         </li>
-        <li className="text-xl pr-7">
-          <Link href={"/rules"}>Rules</Link>
+        <li>
+          <Link href="/booking" className="text-xl hover:text-blue-700 cursor-pointer">
+            Tickets
+          </Link>
+        </li>
+        <li>
+          <Link href="/stages" className="text-xl hover:text-blue-700 cursor-pointer">
+            Stages
+          </Link>
         </li>
       </ul>
     </div>
