91fbad88861bf45f79dccc404df6c81bc2ad6836
Troels <troelsdankert@gmail.com>
2024-05-26 18:20:58 +0200

    logo

--- src/app/components/HeroText.jsx
+++ src/app/components/HeroText.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Image from "next/image";
+
 
 function HeroText() {
   return (
-    <div className="flex text-bono-10 gap-3 mt-8 mb-12 justify-center items-center">
-      <div className="text-8xl font-semibold font-bebas">Foofest´24</div>
-      <div className="w-38 font-montserrat">
-        Vi ses til dans, sang, <br></br> kærlighed og magi på <br></br> Foofest 2024!
-      </div>
-    </div>
+    <div className="flex justify-center items-center mb-8">
+    <Image src="/Foofest-logo-2.png" alt="Logo" width={400} height={400} />
+  </div>
+  
   );
 }
 
