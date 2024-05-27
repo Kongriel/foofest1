e685f43542a795b6b490cb0481551bffd161411a
Troels <troelsdankert@gmail.com>
2024-05-27 09:49:40 +0200

    nye keyframes

--- src/app/components/HeroText.jsx
+++ src/app/components/HeroText.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 function HeroText() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex items-center w-full  overflow-hidden">
       <Image
         src="/Foofest-logo-2.png"
         alt="Logo"
         width={400}
         height={400}
-        className="transform transition duration-800 ease-in-out animate-pulse"
+        className="transform transition duration-800 ease-in-out animate-move"
       />
     </div>
   
