eba5189a416901ebfea065637c4faf8989f1ca62
Anton <krauseriel@gmail.com>
2024-05-28 16:06:01 +0200

    ye

--- src/app/page.js
+++ src/app/page.js
@@ -20,17 +20,17 @@ export default function Home() {
         <Herotekst />
         <Hero />
       </section>
-      <div className="flex justify-center mt-16 mb-44">
+      <div className="flex justify-center mt-16 mb-36">
         <Knap1 />
       </div>
       <section>
         <Centertext />
       </section>
-      <section className="flex mb-16 flex-wrap justify-center items-center gap-8 w-fit p-12 mx-auto">
+      <section className="flex mb-12 flex-wrap justify-center items-center gap-8 w-fit p-12 mx-auto">
         <Card title="FOOFEST 24" status="REGULAR" price="799,00" ticketType="regular" />
         <Card title="FOOFEST 24" status="VIP" price="1299,00" ticketType="VIP" />
       </section>
-      <section className="flex justify-center mt-6 mb-32">
+      <section className="flex justify-center mt-6 mb-24">
         <Knap2 />
       </section>
       <section>
