93842d2a2acad9ca175e6edcae8ce5fdab4d8c2b
Anton <krauseriel@gmail.com>
2024-05-30 01:35:32 +0200

    aria

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -12,16 +12,16 @@ const Footer = () => {
   return (
     <footer className="flex justify-evenly items-center h-20 bg-knap-10 w-full shadow-md opacity-90 px-4">
       <Link href="#" onClick={handleScrollToTop}>
-        <div className="flex-shrink-0 ml-4 cursor-pointer">
+        <div className="flex-shrink-0 cursor-pointer">
           <Image src="/Foofest-logo-2.png" alt="Footer Image" width={80} height={80} />
         </div>
       </Link>
 
       <Link href="/booking" className="flex-grow text-center flex justify-center">
-        <button className="px-6 py-2 text-lg font-medium text-white bg-bono-10 rounded-md hover:bg-blue-700 transition duration-300 font-montserrat animate-bounce">Find billetter</button>
+        <button className="px-8 py-4 md:text-base font-medium text-white bg-bono-10 rounded-md hover:bg-blue-700 transition duration-300 font-montserrat animate-bounce">Find billetter</button>
       </Link>
 
-      <div className="flex-shrink-0 mr-4">
+      <div className="flex-shrink-0 ">
         <p className="text-bono-10 font-montserrat">Vi ses til FooFest '24!</p>
       </div>
     </footer>
